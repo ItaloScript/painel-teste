a4d9f81ca70e3fb604cb7961a1ae5ddcba61e0b9
italoScript <italoyoupw@gmail.com>
2022-07-06 12:51:34 -0300

    now interval works in each table

--- src/pages/result/components/TableOrders.tsx
+++ src/pages/result/components/TableOrders.tsx
@@ -2,13 +2,39 @@ import * as React from 'react';
 import './TableOrders.css'
 
 export interface ITableOrdersProps {
-    orders: any[];
     title: string;
     bgColorHeader?: string;
     colorHeader?: string;
+    filial: string;
+    urlName: string;
+    qtdItems: string;
 }
 
-export default function TableOrders ({ orders = ['',''], title, colorHeader, bgColorHeader}: ITableOrdersProps)  {
+export default function TableOrders ({ title, colorHeader, bgColorHeader, urlName, filial, qtdItems}: ITableOrdersProps)  {
+
+    const pageSelected = React.useRef(1)
+    const [orders, setOrders] = React.useState([])
+
+    React.useEffect(()=>{
+
+
+        const interval = setInterval(async()=>{
+            
+            const url = `http://${import.meta.env.VITE_BACKEND_URL}/get/painelCliente/${urlName}/${filial}/${pageSelected.current}/${qtdItems}`
+            const data = await fetch(url)
+            const result = await data.json()
+
+            pageSelected.current = (pageSelected.current % result.totalPages) + 1
+            setOrders(result.orders)
+
+        },4000)
+
+        return ()=>{
+            clearInterval(interval)
+        }
+
+    },[])
+
   return (
     <div className='container-cards-orders'>
         <div className='container-header-order' style={{
--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,59 +1,56 @@
 import * as React from 'react';
 import TableOrders from './components/TableOrders';
 import './index.css'
-interface IResultProps {
+
+
+
+
+const Result: React.FunctionComponent = () => {
+
+
+
+
+function getUrl(name: string) {
+    const filial = url[2]
+    const items = url[3]
+    const page = url[4]
+    return `http://${import.meta.env.VITE_BACKEND_URL}/get/painelCliente/${name}/${filial}/${page}/${items}`
 }
 
-const Result: React.FunctionComponent<IResultProps> = (props) => {
-
-    const [data, setData] = React.useState<any>({
-        separacao: [
-            '324252'
-        ],
-        caixa: [],
-        expedicao: [],
-    });
-
-    React.useEffect(() => {
-        //get the url and split numbers
-        const url = window.location.pathname.split('/')
-        const filial = url[2]
-        const items = url[3]
-        const page = url[4]
-
-        //get the data from the api
-
-        const getData = async () => {
-            const separacao = (await fetch(`http://${import.meta.env.VITE_BACKEND_URL}/get/painelCliente/separacao/${filial}/${page}/${items}`)).json()
-            const caixa = (await fetch(`http://${import.meta.env.VITE_BACKEND_URL}/get/painelCliente/caixa/${filial}/${page}/${items}`)).json()
-            const expedicao = (await fetch(`http://${import.meta.env.VITE_BACKEND_URL}/get/painelCliente/expedicao/${filial}/${page}/${items}`)).json()
-            
-            setData({
-                separacao,
-                caixa,
-                expedicao
-            })
-        }
-
-        const interval = setInterval(()=>{
-            // getData()
-            console.log('requisição')
-        },1000)
-
-        return ()=>{
-            clearInterval(interval)
-        }
+const url = window.location.pathname.split('/')
+const filial = url[2]
+const items = url[3]
 
 
-    },[])
-
-  return (
+return (
     <div className='containers-orders'>
-        <TableOrders title='Separação' orders={data.separacao} bgColorHeader={"#060606"} colorHeader="#fff"/>
-        <TableOrders title='Caixa' orders={data.caixa} bgColorHeader={"#2f8fc7"} colorHeader="#fff"/>
-        <TableOrders title='Expedição' orders={data.expedicao} bgColorHeader={"#23ab2c"} colorHeader="#fff"/>
+        <TableOrders
+            title='Separação'
+            bgColorHeader={"#060606"}
+            colorHeader="#fff"
+            filial={filial}
+            urlName="separacao"
+            qtdItems={items}
+
+             />
+        <TableOrders
+            title='Caixa'
+            bgColorHeader={"#2f8fc7"}
+            colorHeader="#fff"
+            filial={filial}
+            urlName="caixa"
+            qtdItems={items}
+        />
+        <TableOrders
+            title='Expedição'
+            bgColorHeader={"#23ab2c"}
+            colorHeader="#fff"
+            filial={filial}
+            urlName="expedicao"
+            qtdItems={items}
+        />
     </div>
-  );
+)
 };
 
 export default Result;
